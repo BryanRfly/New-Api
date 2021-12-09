9e73ca5d7a84f2f491779fc076f2d798d4c3655c
Bryan Raflyansah <76269148+BryanRfly@users.noreply.github.com>
2021-12-09 15:35:52 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -22,6 +22,9 @@ router.get('/register', (req, res) => {
     res.sendFile(__path + '/views/register.html')
 })
 
+router.get('/login', (req, res) => {
+res.sendFile(__path + '/views/login.html')
+})
 
 
 module.exports = router

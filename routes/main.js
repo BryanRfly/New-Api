7ef344dbc9ea5084c326da279be81c38065b9720
Bryan Raflyansah <76269148+BryanRfly@users.noreply.github.com>
2021-12-28 07:40:33 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -4,35 +4,30 @@ let express = require('express');
 let router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendFile(__path + '/views/503.html')
+    res.sendFile(__path + '/views/home.html')
 })
 router.get('/docs', (req, res) => {
-    res.sendFile(__path + '/views/503.html')
+    res.sendFile(__path + '/views/index.html')
 })
 router.get('/room-chat', (req, res) => {
-    res.sendFile(__path + '/views/503.html')
+    res.sendFile(__path + '/views/chat.html')
 })
 router.get('/about', (req, res) => {
-    res.sendFile(__path + '/views/503.html')
+    res.sendFile(__path + '/views/about.html')
 })
 router.get('/ttt', (req, res) => {
     res.sendFile(__path + '/views/tictactoe.php')
 })
 router.get('/test', (req, res) => {
-    res.sendFile(__path + '/views/503.html')
+    res.sendFile(__path + '/views/test.html')
 })
 router.get('/register', (req, res) => {
-    res.sendFile(__path + '/views/503.html')
+    res.sendFile(__path + '/views/register.html')
 })
 
 router.get('/login', (req, res) => {
-res.sendFile(__path + '/views/503.html')
+res.sendFile(__path + '/views/login.html')
 })
 
-router.use(function (req, res) {
-     res.status(404)
-    .set("Content-Type", "text/html")
-    .sendFile(__path + '/views/503.html');
-});
 
 module.exports = router
